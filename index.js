9e7d5b55d528e388d8e82de8da6fcb414ff3ed37
Andra <andra.bogoi@gmail.com>
2021-04-15 16:39:03 +0300

    add different ncco

--- index.js
+++ index.js
@@ -21,55 +21,83 @@ const {
 
 */
 
+/**
+ *
+ * This function is meant to handle all the asyncronus event you are gonna receive from conversation api
+ *
+ * it has 2 parameters, event and nexmo context
+ * @param {object} event - this is a conversation api event. Find the list of the event here: https://jurgob.github.io/conversation-service-docs/#/customv3
+ * @param {object} nexmo - see the context section above
+ * */
 
-const DATACENTER = `https://api.nexmo.com` 
-
-const voiceEvent = async (req, res, next) => {
-    const { logger, csClient } = req.nexmo;
-
-    try { 
-        
-        res.json({})
-
-    } catch (err) {
-        
-        logger.error("Error on voiceEvent function")
-    }
-    
+const DATACENTER = `https://api.nexmo.com`;
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
+const voiceEvent = async (req, res, next) => {
+  const { logger, csClient, storageClient } = req.nexmo;
+
+  try {
+    logger.info("voiceEvent", req.body, req.query);
+    res.json({});
+  } catch (err) {
+    logger.error("Error on voiceEvent function");
+  }
+};
 
 const voiceAnswer = async (req, res, next) => {
-    const { logger, csClient } = req.nexmo;
-    logger.info("req", { req_body   : req.body})
-    try {
-        return res.json([
-            {
-                "action": "talk",
-                "text": `Hello , This Is an NCCO Demo`
+  const { logger, csClient } = req.nexmo;
+  logger.info("req", { req_body: req.body });
+  try {
+    return res.json([
+      {
+        action: "connect",
+        from: "447451276946", //your_lvn
+        endpoint: [
+          {
+            type: "phone",
+            number: "447599488246",
+            onAnswer: {
+              url: "http://23.22.82.10:8086/webhooks/agentWhisper/ad_1",
             },
-            {
-                "action": "talk",
-                text: `Your number is ${req.body.from.split("").join(" ")}`
-            },
-            {
-                "action": "talk",
-                text: `And you are colling the number ${req.body.to.split("").join(" ")}`
-            },
-            {
-                "action": "talk",
-                text: `Have a nice day, now we are gonna hangup`
-            }
-        ])
+          },
+        ],
+      },
+    ]);
+  } catch (err) {
+    logger.error("Error on voiceAnswer function");
+  }
+};
 
-    } catch (err) {
+const rtcEvent = async (event, { logger, csClient, storageClient }) => {
+  try {
+  } catch (err) {
+    logger.error({ err }, "Error on rtcEvent function");
+  }
+};
 
-        logger.error("Error on voiceAnswer function")
-    }
+/**
+ *
+ * @param {object} app - this is an express app
+ * you can register and handler same way you would do in express.
+ * the only difference is that in every req, you will have a req.nexmo variable containning a nexmo context
+ *
+ */
+const route = (app) => {
+  app.get("/hello", async (req, res) => {
+    const { logger } = req.nexmo;
 
-}
+    logger.info(`Hello Request HTTP `);
 
+    res.json({
+      text: "Hello Request!",
+    });
+  });
+};
 
 module.exports = {
-    voiceEvent,
-    voiceAnswer
-}
+  voiceAnswer,
+  voiceEvent,
+  rtcEvent,
+  route,
+};
